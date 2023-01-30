63a3ea4755314a19e334dd2349836a347f4beb5e
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2023-01-30 21:25:03 +0000

    Final submit

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,22 +5,24 @@ namespace FirstMVCApp.Controllers
 {
     public class HelloWorldController : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
         public IActionResult Index()
         {
-            DogViewModel doggo = new DogViewModel() 
-            { Name = "Sif", Age = 2 };
-            return View(doggo);
+            
+            return View(dogs);
         }
 
         public IActionResult Create()
         {
             var dogVm = new DogViewModel();
-            return View(dogVm);
+            return View();
         }
 
         public IActionResult CreateDog(DogViewModel dogViewModel)
         {
-            return View("Index"); // Redirected to dog list page
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
         }
 
         public string Hello()
